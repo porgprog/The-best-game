8c1c4106dea2cf82b2175b956c5032b9b410ef2c
porgprog <pporgprog101@gmail.com>
2018-09-22 17:25:59 -0700

    update 1.0.0.0.5

--- New RPG/Assets/PlayerMomvment.cs
+++ New RPG/Assets/PlayerMomvment.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMomvment : MonoBehaviour {
 
+    public Rigidbody rb;
+
+    public float ForwardForce = 2000;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+    }
 }
