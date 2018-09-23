27875470bedc33d1da12773eefb800bb77cee5f4
porgprog <pporgprog101@gmail.com>
2018-09-23 16:09:35 -0700

    1.0.0.0.7

---
+++ New RPG/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ New RPG/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMomvment Momvment;
+
+    private void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obsticle")
+        {
+            Momvment.enabled = false;
+        }
+    }
+}
