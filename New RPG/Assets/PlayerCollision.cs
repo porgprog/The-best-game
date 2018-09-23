a9ab15a8d4d1d795ba5f0488e3c2044d4f09681b
porgprog <pporgprog101@gmail.com>
2018-09-23 12:19:37 -0700

    update 1.0.0.0.6

---
+++ New RPG/Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    void OnCollisionEnter(UnityEngine.Collision collisioninfo)
+    {
+        if (collisioninfo.gameObject.tag == "Obstical")
+        {
+            Debug.Log("We hit an obstical");
+        }
+    }
+}
--- New RPG/Assets/PlayerMomvment.cs
+++ New RPG/Assets/PlayerMomvment.cs
@@ -5,6 +5,8 @@ public class PlayerMomvment : MonoBehaviour {
     public Rigidbody rb;
 
     public float ForwardForce = 2000;
+    public float SideForce = 1000;
+    
 
 	// Use this for initialization
 	void Start ()
@@ -12,9 +14,19 @@ public class PlayerMomvment : MonoBehaviour {
         	
 	}
 	
-	// Update is called once per frame
+	//Using FixedUpdate to mess with physics
 	void FixedUpdate ()
     {
+            // Add a Forward Force
         rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+
+        if( Input.GetKey("d"))
+        {
+            rb.AddForce(SideForce * Time.deltaTime, 0, 0); // Only if condition is met
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SideForce * Time.deltaTime, 0, 0);
+        }
     }
 }
