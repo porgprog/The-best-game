18d3197f330e6fa04c90f888b7ec8346df670b8c
porgprog <pporgprog101@gmail.com>
2018-09-23 19:22:26 -0700

    Branch Start

---
+++ New RPG/Assets/RPG_BOX_Free/Scripts/Managers/DatabaseManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour {
+
+    public static List<Item> Items = new List<Item>(); 
+    public static List<LootTable> LootTables = new List<LootTable>();
+    public static List<Attribute> Attributes = new List<Attribute>();
+
+
+    private void Awake()
+    {
+        Items = new List<Item>(Resources.LoadAll<Item>("ItemsBases")); // Load all items
+        LootTables=new List<LootTable>(Resources.LoadAll<LootTable>("LootTables"));// Load all LootTables
+        Attributes = new List<Attribute>(Resources.LoadAll<Attribute>("Attributes"));// Load all Attributes
+    }
+}
---
+++ New RPG/Assets/RPG_BOX_Free/Scripts/ScriptablObjects/LootTable.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class LootTable : ScriptableObject
+{
+    public string LootTableName;
+    public  List<ItemAndDropChance> Items_DropChance;
+
+    //returns a list of all items in this loot table
+    public List<Item> GetAllItems()
+    {
+        List<Item> ListToReturn = new List<Item>();
+        for (int i = 0; i < Items_DropChance.Count; i++)
+        {
+            ListToReturn.Add(Items_DropChance[i].TheItem);
+        }
+        return ListToReturn;
+    }
+
+    //picks number of items from this loot table and takes into accout its chance
+    public List<Item> GenerateItemsFromLootTable(int HowManyItems)
+    {
+        List<Item> ListToReturn = new List<Item>();
+
+        if (Items_DropChance.Count > 0)
+        {
+            int TotalWeight = 0;
+
+            for (int i = 0; i < Items_DropChance.Count; i++)
+            {
+                TotalWeight += Items_DropChance[i].DropChance;
+            }
+            for (int j = 0; j < HowManyItems; j++)
+            {
+                int TheRandom = Random.Range(1, TotalWeight + 1);
+                int CurrentWeight = TotalWeight;
+
+                for (int i = 0; i < Items_DropChance.Count; i++)
+                {
+                    CurrentWeight -= Items_DropChance[i].DropChance;
+                    if (TheRandom > CurrentWeight)
+                    {
+                        ListToReturn.Add(Items_DropChance[i].TheItem);
+                        break;
+                    }
+                }
+            }
+        }
+        return ListToReturn;
+    }
+
+}
+
+[System.Serializable]
+public class ItemAndDropChance
+{
+    public Item TheItem;
+    public int DropChance;
+
+    public ItemAndDropChance(Item aItem, int aDropChance)
+    {
+        TheItem = aItem;
+        DropChance = aDropChance;
+    }
+}
